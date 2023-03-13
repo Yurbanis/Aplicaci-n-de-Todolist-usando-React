b1138fa0da411b323a1013433b0549c313b68809
Yurbanis <yurbanisbriceno@gmail.com>
2023-03-13 22:51:46 +0000

    Se realiza todo de lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../../styles/index.css";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [listaPalabra, setlistaPalabra] = useState([]);
+
+  const insertar = (item) => {
+    const valorNuevo = { id: listaPalabra.length + 1, name: item };
+    const lista = [...listaPalabra, valorNuevo];
+    setlistaPalabra(lista);
+  };
+
+  const delet = (nameDelete) => {
+    setlistaPalabra(listaPalabra.filter((item) => item.name !== nameDelete));
+  };
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-md-center">
+        <h1 className="text-center tod">todos</h1>
+        <ul className="list-group col-md-5 mt-5">
+          <li className="list-group-item d-flex justify-content-between align-items-center">
+            <input
+              type="text"
+              onKeyUp={(e) =>
+                e.code === "NumpadEnter" || e.code === "Enter" ? insertar(e.target.value) : null
+              }
+              className="form-control"
+              id=""
+            />
+          </li>
+          {listaPalabra.map((listar) => {
+            return (
+              <li
+                key={listar.id}
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                {listar.name}
+                <span
+                  className=""
+                  onClick={() => delet(listar.name)}
+                >
+                  <i className="fas fa-times"></i>
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
